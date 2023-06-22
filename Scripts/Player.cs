70b7196be76dee8007f177d4729d3e03d55b3aa8
Htomsik <JessJake@proton.me>
2023-06-22 23:16:54 +0300

    Initialize

---
+++ Scripts/Player.cs
@@ -0,0 +1,151 @@
+using System;
+using Godot;
+
+
+public class Player : KinematicBody2D
+{
+    #region Const
+
+    private const int MoveSpeed = 10;
+    
+    private const int MaxMoveSpeed = 250;
+    
+    private const int GravitySpeed = 20;
+
+    private const int JumpSpeed = 700;
+
+    private const int levelLimit = 3000;
+    
+    
+    private const string JumpSprite = "Jump";
+    
+    private const string IdleSprite = "Idle";
+    
+    private const string WalkSprite = "Walk";
+    
+    
+    
+    private const string MoveLeft = "PlayerMoveLeft";
+    
+    private const string MoveRight = "PlayerMoveRight";
+
+    private const string MoveJump = "PlayerJump";
+    
+    #endregion
+    
+    #region fields
+
+    private Vector2 _linearVelocity = new Vector2(0, 0);
+
+    private readonly Vector2 _floor = new Vector2(0, -1);
+
+    private AnimatedSprite _sprite;
+    
+    private bool _canDoubleJump = true;
+    #endregion
+
+    #region Methods
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _sprite = (AnimatedSprite)GetNode("Sprite");
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Gravity();
+        
+        Move();
+
+        Jump();
+        
+        MoveAndSlide(_linearVelocity, _floor);
+
+        Animate();
+        
+        base._PhysicsProcess(delta);
+    }
+
+    private void Animate()
+    {
+        if (_linearVelocity.x != 0)
+        {
+            _sprite.FlipH = _linearVelocity.x < 0;
+        }
+        
+        _sprite.Play(!IsOnFloor() ? JumpSprite : WalkSprite);
+
+        if (_linearVelocity.x == 0 && _linearVelocity.y == 0)
+        {
+            _sprite.Play(IdleSprite);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.IsActionJustPressed(MoveJump) && _canDoubleJump)
+        {
+            _canDoubleJump = false;
+            _linearVelocity.y = -JumpSpeed;
+        }
+
+        if (IsOnCeiling())
+        {
+            _linearVelocity.y = 1;
+        }
+
+        if (IsOnFloor())
+        {
+            _canDoubleJump = true;
+        }
+    }
+
+    private void Move()
+    {
+        var highThenMax = Math.Abs(_linearVelocity.x) > MaxMoveSpeed;
+        
+        if (Input.IsActionPressed(MoveLeft) && !highThenMax)
+        {
+            _linearVelocity.x -=  MoveSpeed;
+        }
+        else if (Input.IsActionPressed(MoveRight) && !highThenMax)
+        {
+            _linearVelocity.x  +=  MoveSpeed;
+        }
+        else if (_linearVelocity.x != 0)
+        {
+            _linearVelocity.x += _linearVelocity.x > 0 ? -MoveSpeed : MoveSpeed;
+        }
+    }
+
+    private void Gravity()
+    {
+        if (Position.y >= levelLimit)
+        {
+            EndGame();
+        }
+        
+        if (!IsOnFloor())
+            _linearVelocity.y += GravitySpeed;
+        else
+            _linearVelocity.y = 0;
+    }
+
+    private void EndGame()
+    {
+        GetTree().ChangeScene("res://Scenes/Game over.tscn");
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+        //  public override void _Process(float delta)
+        //  {
+        //      
+        //  }
+    #endregion
+}
+
+    
+    
+
+
